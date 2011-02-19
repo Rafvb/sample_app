b786bddbb7f9128489b3cee3151e0ffc1daed290
Raf Van Baelen <rafvanbaelen@gmail.com>
2011-02-19 14:05:26 +0100

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Raf Van Baelen"
+  user.email                  "rafvanbaelen@gmail.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
